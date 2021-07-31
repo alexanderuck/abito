7ec1e9c1f3fa5a774560b7e4b6f997b69e7c9a1a
alexanderuck <80569120+alexanderuck@users.noreply.github.com>
2021-07-31 13:15:43 +0500

    Add files via upload
    Mobile version

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 10,
-        slidesPerView: 6,
-        freeMode: true,
-        watchSlidesVisibility: true,
-        watchSlidesProgress: true,
-      });
-      var swiper2 = new Swiper(".mySwiper2", {
-        spaceBetween: 10,
-        thumbs: {
-          swiper: swiper,
-        },
-      });
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+})
+menuCloseBtn.addEventListener("click", () => {
+    menu.classList.toggle("is-open");
+})
